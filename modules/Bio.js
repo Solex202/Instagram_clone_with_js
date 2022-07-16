b6e3580550cfe53e6b13475321e0bacc2179909d
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-07-16 20:21:44 +0100

    updated code

---
+++ modules/Bio.js
@@ -0,0 +1,10 @@
+const Bio = () => `
+<section class="bio">
+            <div class="profile-photo">
+                <img src="/assets/lota.jpg" alt="profile-photo" width="130px"/>
+            </div>
+            <div class="profile-info">
+                <p class="username">the_lotachukwu</p>
+                <button class="">follow</button>
+            </div>
+        </section>`
