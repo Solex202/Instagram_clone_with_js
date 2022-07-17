56780a0822c64eed8f274f618d045c4b4ed78941
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-07-17 14:39:15 +0100

    updated code

---
+++ modules/nav/Nav.js
@@ -0,0 +1,15 @@
+const Nav = () => `
+<nav>
+        <a href="#" class="logo">
+            <img src="/assets/insta.png" width="150px"/>
+        </a>
+        <input type="text" class="search" placeholder="Search"/>
+        <span class="nav-links">
+            <a href="#"><i class="fa-solid fa-house"></i></a>
+            <a href="#"><i class="fa-solid fa-message"></i></a>
+            <a href="#"><i class="fa-solid fa-compass"></i></a>
+            <a href="#"><i class="fa-solid fa-heart"></i></a>
+        </span>
+    </nav>`
+
+    export default Nav;
